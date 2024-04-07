1b4d5e0d4c2b59f90d2fe7c07cd53786c53e259c
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2024-04-07 15:57:15 +0300

    create banner http method

--- internal/transport/http/banner.go
+++ internal/transport/http/banner.go
@@ -1,8 +1,10 @@
 package http
 
 import (
+	"avito_intern/internal/models"
 	"avito_intern/internal/transport/http/request"
 	"avito_intern/internal/transport/http/response"
+	"encoding/json"
 	"net/http"
 )
 
@@ -45,4 +47,32 @@ func (t transport) GetBanners(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	t.handleOk(w, banners, methodName, http.StatusOK)
+}
+func (t transport) CreateBanner(w http.ResponseWriter, r *http.Request) {
+	methodName := "CreateBanner"
+
+	var req request.CreateBanner
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		t.handleError(w, err, err, methodName, http.StatusBadRequest)
+
+		return
+	}
+	if err := validate.Struct(req); err != nil {
+		t.handleError(w, err, err, methodName, http.StatusBadRequest)
+		return
+	}
+	banner := models.Banner{
+		Tags:     req.Tags,
+		Feature:  req.Feature,
+		Content:  req.Content,
+		IsActive: req.IsActive,
+	}
+	id, err := t.s.CreateBanner(r.Context(), banner)
+	if err != nil {
+		t.handleError(w, err, err, methodName, http.StatusInternalServerError)
+
+		return
+	}
+	t.handleOk(w, response.CreateBanner{id}, methodName, http.StatusCreated)
 }
