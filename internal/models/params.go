8f6ce7f41c6b6747f5a1bb005d0ff1c9a735faeb
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2024-04-14 11:47:17 +0300

    made new dir for complex queries

--- internal/models/params.go
+++
@@ -1,8 +0,0 @@
-package models
-
-//type GetBannersParams struct {
-//	TagID     int32 `json:"tag_id"`
-//	FeatureID int32 `json:"feature_id"`
-//	Limit     int32 `json:"limit"`
-//	Offset    int32 `json:"offset"`
-//}
